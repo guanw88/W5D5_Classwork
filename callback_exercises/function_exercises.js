877e6cce2640bfc4ea76e3f7dfbd47cb01233cc9
guanw88 <guanw88@gmail.com>
2018-11-23 14:23:51 -0800

    Add clock functionality to Clock class

--- callback_exercises/function_exercises.js
+++ callback_exercises/function_exercises.js
@@ -6,6 +6,7 @@ class Clock {
     this.mins = this.date.getMinutes();
     this.secs = this.date.getSeconds();
     this.printTime(this.hours, this.mins, this.secs);
+    setInterval(this._tick.bind(this), 1000);
   }
 
   printTime() {
@@ -14,6 +15,42 @@ class Clock {
     let secStr = ("0" + this.secs).slice(-2);
 
     console.log(`${hrStr}:${minStr}:${secStr}`);
+  }
+
+  countSeconds(sec) {
+    if (sec + 1 === 60) {
+      return 0;
+    } else {
+      return sec + 1;
+    }
+  }
+
+  countMinutes(min) {
+    if (min + 1 === 60) {
+      return 0;
+    } else {
+      return min + 1;
+    }
+  }
+
+  countHours(hour) {
+    if (hour + 1 === 24) {
+      return 0;
+    } else {
+      return hour + 1;
+    }
+  }
+
+  _tick() {
+    console.clear();
+    this.secs = this.countSeconds(this.secs);
+    if (this.secs === 0) {
+      this.mins = this.countMinutes(this.mins);
+    }
+    if (this.mins === 0) {
+      this.hours = this.countHours(this.hours);
+    }
+    this.printTime();
   }
 
 }
